7c168b557888d22db45075934297e885f17c7b2e
Apoorv Saxena <apoorvcr@gmail.com>
2017-08-31 03:55:02 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ap00rvSaxena.github.io
---
+++ js/google_map.js
@@ -0,0 +1,38 @@
+/* Google map
+            ------------------------------------------------*/
+            var map = '';
+            var center;
+
+            function initialize() {
+                var mapOptions = {
+                    zoom: 12,
+                    center: new google.maps.LatLng(23.283600, 77.355091),
+                    scrollwheel: false
+                };
+            
+                map = new google.maps.Map(document.getElementById('google-map'),  mapOptions);
+
+                google.maps.event.addDomListener(map, 'idle', function() {
+                  calculateCenter();
+                });
+            
+                google.maps.event.addDomListener(window, 'resize', function() {
+                  map.setCenter(center);
+                });
+            }
+
+            function calculateCenter() {
+                center = map.getCenter();
+            }
+
+            function loadGoogleMap(){
+                var script = document.createElement('script');
+                script.type = 'text/javascript';
+                script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
+                document.body.appendChild(script);
+            }
+        
+            // DOM is ready
+            $(function() {                
+                loadGoogleMap(); // Google Map
+            });
