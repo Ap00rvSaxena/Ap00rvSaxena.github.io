7157bdbf49f1b72d3adce77f86f6ebd1df0d8ae2
Apoorv Saxena <apoorvcr@gmail.com>
2017-08-31 04:02:47 +0530

    Map Update

--- js/google_map.js
+++ js/google_map.js
@@ -28,7 +28,7 @@
             function loadGoogleMap(){
                 var script = document.createElement('script');
                 script.type = 'text/javascript';
-                script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
+                script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDdjV4gub5gZWf0ZRzfToRIF7onj7kFU4U&' + 'callback=initialize';
                 document.body.appendChild(script);
             }
         
